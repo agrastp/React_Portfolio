1199fa6483bf2f02183641a1ee7913186db90001
AgrastP <agrastp@yahoo.com>
2024-01-27 17:09:43 -0500

    worked on form page and added resume download button

--- React_Portfolio/src/pages/ResumePage.jsx
+++ React_Portfolio/src/pages/ResumePage.jsx
@@ -1,9 +1,16 @@
+import Resume from '../assets/files/resume.pdf'
+
+
 export default function ResumePage() {
-    // The Navbar component will render each of the Link elements in the links prop
-    return (
-      <>
+  // The Navbar component will render each of the Link elements in the links prop
+  return (
+    <>
       <h1>Resume Page</h1>
-      
-      </>
-    );
-  }
+
+
+      <a href={Resume} download="Resume" target='_blank'>
+        <button className="resume">Download Resume</button>
+      </a>
+    </>
+  );
+}
